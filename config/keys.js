2ea0148f5a87ed3ceecd7f3e61f2c7a07f60fa81
Air_Kano <15737228935@163.com>
2020-10-15 11:40:26 +0800

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb://nodevueelement:test1234@ds223756.mlab.com:23756/node-vue-element',
+  secretOrKey: 'secret'
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require("path");
+const app = express();
+
+// 处理静态文件
+app.use(express.static('client/dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
+
+// 引入users.js
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to mongodb
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// passport 初始化
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+// app.get("/",(req,res) => {
+//   res.send("Hello World!");
+// })
+
+// 使用routes
+app.use('/api/users', users);
+app.use('/api/profile', profiles);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
